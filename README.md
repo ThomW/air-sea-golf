a615fc9e63ea0d8498ab8ba0170d58d2bf01fd9c
Thom Wetzel <thom.wetzel@gmail.com>
2018-02-11 00:58:28 -0500

    Game Over. Added scoring, explosions, game over screen, shooter bug fixes, tons of little things

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # air-sea-golf
-Something dumb I'm working on for the Shacknews Slow Jam Game Jam (https://itch.io/jam/shacknews-slow-jam)
+Something dumb I created for the Shacknews Slow Jam Game Jam (https://itch.io/jam/shacknews-slow-jam)
 
 This requires phaser.js and its annoyingly commercial Box2d plugin.
 http://phaser.io/
+
+CREDITS
+
+Horrible concept and execution by Thom Wetzel / www.lmnopc.com
+
+[Phaser.io](http://phaser.io/)
+
+[Box2D](http://box2d.org/) by Erin Catto
+
+Hill generation code ported adapted from http://www.emanueleferonato.com/2011/07/14/create-a-terrain-like-the-one-in-tiny-wings-with-flash-and-box2d/
+
+Atari Classic TrueType Font Created by Mark Simonson / [Mac/Atari Fusion--Atari Home Computer Resources](http://members.bitstream.net/marksim/atarimac/fonts.html)
+
--- game.js
+++ game.js
@@ -5,6 +5,7 @@ var soundNames = ['shoot', 'inhole', 'planecollide', 'ballbounce', 'planebounce'
 var sounds = [];
 
 function preload () {
+
    game.load.image('background', 'img/background.png');
    game.load.image('title', 'img/title.png');
    game.load.image('plane', 'img/plane.png');
@@ -15,8 +16,16 @@ function preload () {
    game.load.image('scanlines', 'img/scanlines.png');
    game.load.image('tv', 'img/tv-overlay.png');
 
-   game.load.image('scoreFont', 'fonts/asb.png');
+   if (game.device.desktop) {
+      game.load.spritesheet('gameover', 'img/game-over.png', 734, 46);
 
+   } else {
+      game.load.spritesheet('gameover', 'img/game-over-mobile.png', 627, 46);
+   }
+
+   game.load.spritesheet('explosion', 'img/explosion', 64, 32);
+
+   game.load.image('scoreFont', 'fonts/asb.png');
 
    for (var i = 0; i < soundNames.length; i++) {
       game.load.audio(soundNames[i], 'audio/' + soundNames[i] + '.mp3');
@@ -36,18 +45,18 @@ var shooterAngleSprite;
 
 var mouseDownPosition = {};
 
-var groundLevel = 600 - 128; // platform is 128 high, centered at bottom of screen
+var groundLevel = 600 - 128; 
 
  var planes;
 
 var hillGraphics = [];
-var hillBodies = [];
+var hillBody = null;
 
 var tv;
 var scanlines;
 
 // Some game variables
-var MAX_BALLS = 3;
+var MAX_BALLS = 5;
 var score;
 var remainingBalls = 0;
 
@@ -55,7 +64,10 @@ var GAME_STATE_BUSY = 0;
 var GAME_STATE_TITLE = 1;
 var GAME_STATE_PLAYING = 2;
 var GAME_STATE_CHANGE_HOLES = 8;
+var GAME_STATE_END_GAME = 15;
+var GAME_STATE_GAME_OVER = 16;
 
+var lastGameState = null;
 var gameState = GAME_STATE_TITLE;
 
 var shootingInput = null;
@@ -64,30 +76,48 @@ var score;
 var scoreText;
 var scoreImg;
 
+var ballRestingData = null;
 
 function gameStart() {
 
    drawHills(false);
 
-   score = 0;
-
    showTitle();
-
-   // Start the game when the mouse is clicked
-   gameState = GAME_STATE_TITLE;
-   game.input.onDown.add(clickStart, this);
 }
 
 function clickStart() {
+
+   score = 0;
 
    gameState = GAME_STATE_PLAYING;
 
    // Remove the click event that starts the game
    game.input.onDown.removeAll();
 
-   hideTitle();
+   // Hide the title
+   game.add.tween(title).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+
+   // Hide the game over sprite
+   gameoverSprite.visible = false;
 
    roundStart();
+}
+
+function gameOver() {
+
+   showTitle();
+
+}
+
+function showTitle() {
+
+   gameoverSprite.visible = true;
+
+   game.add.tween(title).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+
+   // Start the game when the mouse is clicked
+   gameState = GAME_STATE_TITLE;
+   game.input.onDown.add(clickStart, this);
 }
 
 function drawHills(showHole) {
@@ -100,9 +130,9 @@ function drawHills(showHole) {
    while (hillGraphics.length > 0) {
       hillGraphics.pop().destroy();
    }
-   while (hillBodies.length > 0) {
-      hillBodies.pop().destroy();
-   }
+
+   if (hillBody != null) { hillBody.destroy(); }
+   
    flagSprite.kill();
    holeSensor.kill();
 
@@ -144,8 +174,6 @@ function drawHills(showHole) {
          else if (hillCoordinates[holeIdx][5] >= hillCoordinates[holeIdx + 1][5]) {
             wentDown = 0;
          }
-
-
       }
 
       // Place the flag
@@ -155,7 +183,26 @@ function drawHills(showHole) {
       holeSensor.reset(flagPoint.x, game.world.height);
    }
 
+
+   // Make the shooter touch the ground
+   var shooterBottomX = shooterBaseSprite.world.x + (shooterBaseSprite.width * 0.5);
+   var shooterY;
+   for (var i = 0; i < hillCoordinates.length; i++) {
+      // Find the first hillCoordinates under the center of the shooter base
+      if (hillCoordinates[i][0] >= shooterBottomX) {
+         shooterY = hillCoordinates[i][3] - 6;
+         shooterBaseSprite.reset(shooterBaseSprite.world.x, shooterY);
+         shooterAngleSprite.reset(shooterBaseSprite.world.x, shooterY);
+         break;
+      }
+   }
+
+   var tallestHill = game.world.height;
+
    // 
+   hillBody = new Phaser.Physics.Box2D.Body(game, null, 0, 0);
+   hillBody.static = true;
+
    for (var i = 0; i < hillCoordinates.length; i++)
    {
       // Clone the hillCoordinates of this point so I can alter them to make them look blocky without affecting the physics
@@ -166,26 +213,42 @@ function drawHills(showHole) {
       poly = new Phaser.Polygon(graphicPoints);
       graphics = game.add.graphics(0,0);
       graphics.beginFill(0x008800);
+      
+      /*
+      // Paints every 10th hill slice gray for debugging
+      if (i % 10 == 0) { graphics.beginFill(0x888888); }
+      */
+
       graphics.drawPolygon(poly.points);
       graphics.endFill();
 
-      var body = new Phaser.Physics.Box2D.Body(game, null, 0, 0);
-      body.setChain(hillCoordinates[i]);
-      body.static = true;
-
-      // Store the graphics and body objects so they can be destroyed next redraw
+      // Store the graphics
       hillGraphics.push(graphics);
-      hillBodies.push(body);
+
+      // Add the polygon to the hillBody object
+      //
+      // The area under the shooter is a flat plane to avoid issues with the ball getting stuck at launch
+      if (hillCoordinates[i][4] <= shooterBottomX) {
+         var shooterBaseCoordinates = hillCoordinates[i].slice(0);
+         shooterBaseCoordinates[3] = shooterY;
+         shooterBaseCoordinates[5] = shooterY;
+         hillBody.addPolygon(shooterBaseCoordinates, 0, 4);         
+
+      } else {
+         hillBody.addPolygon(hillCoordinates[i], 0, 4);
+      }
+
+      // Find the highest peak's Y coordinate
+      if (hillCoordinates[i][3] < tallestHill) {
+         tallestHill = hillCoordinates[i][3];
+      }
    }
 
-   // Make the shooter touch the ground
-   var shooterBottomX = shooterBaseSprite.world.x + (shooterBaseSprite.width * 0.5);
-   for (var i = 0; i < hillCoordinates.length; i++) {
-      // Find the first hillCoordinates under the center of the shooter base
-      if (hillCoordinates[i][0] >= shooterBottomX) {
-         shooterBaseSprite.reset(shooterBaseSprite.world.x, hillCoordinates[i][3] - 6);
-         shooterAngleSprite.reset(shooterBaseSprite.world.x, hillCoordinates[i][3] - 6);
-         break;
+   // Make sure none of the planes are trapped in the landscape after a redraw
+   for (var i = 0, len = planes.children.length; i < len; i++) {
+      var p = planes.children[i];
+      if (p.world.y >= tallestHill) {
+         resetPlane(p);
       }
    }
 
@@ -223,29 +286,26 @@ function updateScore() {
       }
    }
 
-   scoreText.text = tmp + ' ' + score;
-
+   scoreText.text = tmp + ('   ' + score).slice(-4);
 }
 
 function create () {
 
-   // Enable Physics
+      // Enable Physics
    game.physics.startSystem(Phaser.Physics.BOX2D);
    game.physics.box2d.setBoundsToWorld();
 
    game.physics.box2d.density = 5;
    game.physics.box2d.friction = 100;
-   game.physics.box2d.restitution = 0.25; // 0-1 (No bounce - max bounce)
-
    game.physics.box2d.gravity.y = 6000;
-
+   game.physics.box2d.density = 4;
 
    var background = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
    background.anchor.setTo(0.5, 0.5);
 
    scoreText = game.add.retroFont('scoreFont', 7, 6, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ,.', 10);
    scoreText.text = '';
-   scoreImg = game.add.image(game.world.centerX, 50, scoreText);
+   scoreImg = game.add.image(game.world.centerX, 70, scoreText);
    scoreImg.anchor.setTo(0.5, 0.5);
    scoreImg.scale.x = 10;
    scoreImg.scale.y = 10;
@@ -257,45 +317,35 @@ function create () {
    tv = game.add.sprite(game.world.centerX, game.world.centerY, 'tv');
    tv.anchor.setTo(0.5, 0.5);
 
+   gameoverSprite = game.add.sprite(game.world.centerX, game.world.centerY + 20, 'gameover');
+   gameoverSprite.animations.add('gameover');
+   gameoverSprite.animations.play('gameover', 0.5, true);
+   gameoverSprite.anchor.setTo(0.5, 0.5);
+   gameoverSprite.alpha = 0.75;
+
    // Setup planes using group
    planes = game.add.group();
    planes.enableBody = true;
    planes.physicsBodyType = Phaser.Physics.BOX2D;
 
    for (var i = 0; i < 5; i++) {
-
-      var startX = 0;
-      var startY = groundLevel - (80 * (i + 1));
-      var velocityX = Math.random() * 100 + 50;
-      var scaleX = 1;
-
-      if (Math.round(Math.random())) {
-         startX = game.width;
-         velocityX *= -1;
-         scaleX *= -1;
-      }
-
-      var p = planes.create(startX, startY, 'plane');
+      var p = planes.create(0, 0, 'plane');
+      p.body.gravityScale = 0;
+      p.body.collideWorldBounds = false;
+      p.body.setCollisionCategory(3);
 
       // Custom variable to hold the plane's starting position to make it easier to reset
-      p.startingY = startY;
+      p.startingY = groundLevel - (80 * (i + 1));
 
-      p.tint = Math.random() * 0xffffff;
-      p.scale.x = scaleX;
-
-      p.body.velocity.x = velocityX;
-
-      p.body.gravityScale = 0;
-
-      p.body.collideWorldBounds = false;
-
-      p.body.setCollisionCategory(3);
+      resetPlane(p);
 
       // p.body.setCategoryContactCallback(1, planeOnPlaneCallback, this);
    }
 
-   // Increase default density before creating ball
-   game.physics.box2d.density = 4;
+   //  An explosion pool
+   explosions = game.add.group();
+   explosions.createMultiple(planes.children.length, 'explosion');
+   explosions.forEach(setupExplosion, this);
 
    // Arrow sprite for aiming
    arrowSprite = game.add.sprite(100, groundLevel - 100, 'arrow');
@@ -323,7 +373,11 @@ function create () {
    ballSprite.body.mass = 100;
    ballSprite.body.allowSleep = true;
    ballSprite.body.bullet = true;
+   ballSprite.body.restitution = 0.25;
    ballSprite.kill();
+
+   ballSprite.body.bodyDef.linearDamping = 1;
+   ballSprite.body.bodyDef.angularDamping = 1;
 
    // Collisions between bodies and groups have to use CategoryContact to work
    ballSprite.body.setCategoryContactCallback(3, ballOnPlaneCallback, this);
@@ -348,6 +402,8 @@ function create () {
    shooterAngleSprite.pivot.y = 6;
    shooterAngleSprite.angle = -75;
 
+   shooterBaseSprite.tint = shooterAngleSprite.tint = 0x0000ff;
+
    // Init sounds
    for (var i = 0; i < soundNames.length; i++) {
       sounds[soundNames[i]] = game.add.audio(soundNames[i]);
@@ -358,16 +414,26 @@ function create () {
 
 function update() {
 
-   if (gameState == GAME_STATE_CHANGE_HOLES) {
+   // Only execute 
+   if (gameState != lastGameState) {
 
-      gameState = GAME_STATE_BUSY;
+      if (gameState == GAME_STATE_CHANGE_HOLES) {
 
-      score += 1;
+         gameState = GAME_STATE_BUSY;
 
-      roundStart();
+         score += 1;
 
-      gameState = GAME_STATE_PLAYING;
+         roundStart();
+
+         gameState = GAME_STATE_PLAYING;
+      } 
+      else if (gameState == GAME_STATE_END_GAME) {
+
+         gameOver();
+
+      }
    }
+   lastGameState = gameState;
 
    // When the player is shooting, update the shooter
    if (shootingInput) {
@@ -376,8 +442,12 @@ function update() {
 
    // Do some housekeeping on the planes
    for (var i = 0, len = planes.children.length; i < len; i++) {
-   
+
       var p = planes.children[i];
+
+      if (p.doReset) {
+         resetPlane(p);
+      }
       
        // Wrap the planes if they hit the edge of the world
       if (p.body.velocity.x > 0 && p.world.x > game.world.width) {
@@ -386,59 +456,108 @@ function update() {
          p.body.reset(game.world.width, p.world.y);
       }
 
-      // Reset planes that go off screen
+
+      // Reset planes that go off screen or that get stuck on something
       if (p.world.y < 0) {
-         
-         var startX = 0;
-         var velocityX = Math.random() * 100 + 50;
-         var scaleX = 1;
-
-         if (Math.round(Math.random())) {
-            startX = game.width;
-            velocityX *= -1;
-            scaleX *= -1;
-         }
-
-         // Flip the texture the correct way
-         p.scale.x = Math.abs(p.scale.x) * scaleX;
-
-         // Reset the position, angle, and velocities
-         p.body.reset(startX, p.startingY);
-         p.body.angle = 0;
-         p.body.velocity.x = velocityX;
-         p.body.velocity.y = 0;
-         p.body.angularVelocity = 0;
-
-         // Recolor the plane
-         p.tint = Math.random() * 0xffffff;
+         resetPlane(p);
       }
+   }
+
+   // Kill the ballSprite if it goes out of bounds
+   if (ballSprite.world.x > game.world.width || ballSprite.world.x < 0) {
+      ballSprite.kill();
+   }
+
+   // Homebrew at rest
+   var POSITION_THRESHOLD = 2;
+   var POSITION_TIMEOUT = 2000;
+   if (ballSprite.alive && Math.abs(ballSprite.deltaX) < POSITION_THRESHOLD && Math.abs(ballSprite.deltaY) < POSITION_THRESHOLD) {
+      if (ballRestingStart == null) {
+         ballRestingStart = game.time.time;
+      } else if (game.time.elapsedSince(ballRestingStart) > POSITION_TIMEOUT) {
+         // Thresholds for time and position have been exceeded - put the ball to sleep
+         ballSprite.body.gravityScale = 0;
+         ballSprite.body.velocity.x = 0;
+         ballSprite.body.velocity.y = 0;
+         ballSprite.body.angularVelocity = 0;
+         ballSprite.kill();
+         ballSprite.visible = true;
+         ballRestingStart = null;
+      }
+   } else {
+      ballRestingStart = null;
+   }
+
+   // If the ballSprite is disabled and the player doesn't have any more balls, end the game
+   if (!ballSprite.alive && remainingBalls == 0) {
+      gameState = GAME_STATE_END_GAME;
    }
 }
 
-function showTitle() {
-   game.add.tween(title).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+function explosionLooped(sprite, animation) {
+   if (animation.loopCount >= 5) {
+      animation.loop = false;
+   } else {
+      colors = [0xff8800, 0xffff00, 0xff0000, 0xffcc00, 0xff8800];
+      sprite.tint = colors[animation.loopCount % colors.length];
+   }
 }
 
-function hideTitle() {
-   game.add.tween(title).to( { alpha: 0 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+function setupExplosion(explosion) {
+   explosion.anchor.x = 0.5;
+   explosion.anchor.y = 0.5;
+   explosion.tint = 0xff8800;
+
+   anim = explosion.animations.add('explosion');
+
+   anim.onLoop.add(explosionLooped, this);
+}
+
+function resetPlane(p) {
+
+   p.doReset = false;
+
+   var startX = 0;
+   var velocityX = Math.random() * 100 + 50;
+   var scaleX = 1;
+
+   if (Math.round(Math.random())) {
+      startX = game.width;
+      velocityX *= -1;
+      scaleX *= -1;
+   }
+
+   // Flip the texture the correct way
+   p.scale.x = Math.abs(p.scale.x) * scaleX;
+
+   // Reset the plane's health
+   p.health = randInt(2,3);
+
+   // Reset the position, angle, and velocities
+   p.body.reset(startX, p.startingY);
+   p.body.angle = 0;
+   p.body.velocity.x = velocityX;
+   p.body.velocity.y = 0;
+   p.body.angularVelocity = 0;
+
+   // Recolor the plane
+   p.tint = Math.random() * 0xffffff;
 }
 
 function mouseDragStart() {
 
-   /*
    if (remainingBalls <= 0) {
       return;
    }
-   */
+
+   shootingInput = true;
 
    ballSprite.body.gravityScale = 0;
-   ballSprite.reset(100, shooterBaseSprite.world.y - 10);
-   ballSprite.visible = false;
+   ballSprite.reset(shooterBaseSprite.world.x + 4, shooterBaseSprite.world.y);
    ballSprite.body.velocity.x = 0;
    ballSprite.body.velocity.y = 0;
    ballSprite.body.angularVelocity = 0;
-
-   shootingInput = true;
+   ballSprite.visible = false;
 
    mouseDownPosition.x = game.input.activePointer.position.x;
    mouseDownPosition.y = game.input.activePointer.position.y;
@@ -457,7 +576,6 @@ function mouseDragStart() {
 
    remainingBalls -= 1;
    updateScore();
-
 }
 
 function updateShooter() {
@@ -476,7 +594,7 @@ function updateShooter() {
 
    var length = Math.sqrt( dx * dx + dy * dy );
    
-   arrowSprite.scale.set(length * 0.05, 0.5);
+   arrowSprite.scale.set(length * 0.033, 0.5);
    arrowSprite.rotation = Math.atan2( -dy, -dx );
 
    // Move the shooter
@@ -520,11 +638,22 @@ function planeOnPlaneCallback(body1, body2, fixture1, fixture2, begin) {
 function ballOnPlaneCallback(body1, body2, fixture1, fixture2, begin) {
 
     // This callback is also called for EndContact events, which we are not interested in.
-    if (!begin) {
+    if (!begin || !ballSprite.alive) {
         return;
     }
 
     sounds.planecollide.play();
+
+    body2.sprite.health -= 1;
+
+    if (body2.sprite.health <= 0) {
+      
+      var explosion = explosions.getFirstExists(false);
+      explosion.reset(body2.x, body2.y);
+      explosion.play('explosion', 30, true, true);
+
+      body2.sprite.doReset = true;
+    }
 }
 
 function ballOnGroundCallback(body1, body2, fixture1, fixture2, begin) {
@@ -557,9 +686,23 @@ function inHoleCallback(body1, body2, fixture1, fixture2, begin) {
 
 function render() {
 
+   /*
    // This enables the box2D debugger view
-   // game.debug.box2dWorld();
+   game.debug.box2dWorld();
 
+   // Show the plane positions (not really necessary since fixing the hill spawning, but whatever)
+   for (var i = 0, len = planes.children.length; i < len; i++) {
+      var p = planes.children[i];
+       game.debug.text(i + ': ' + Math.round(p.world.x) + ', ' + Math.round(p.world.y), 10, 50 + i * 12);
+   }
+   */
+   
+   /*
+   // ballSprite values for testing my code to put the ball to sleep manually
+   game.debug.text('Alive: ' + ballSprite.alive + ' ' + ('    ' +  Math.round(ballSprite.body.velocity.x)).slice(-4) + ',' + ('    ' + Math.round(ballSprite.body.velocity.y)).slice(-4), 100, 100);
+   game.debug.text(('    ' +  Math.round(ballSprite.world.x)).slice(-4) + ',' + ('    ' + Math.round(ballSprite.world.y)).slice(-4), 100, 120);
+   game.debug.text(('    ' +  Math.round(ballSprite.deltaX)).slice(-4) + ',' + ('    ' + Math.round(ballSprite.deltaY)).slice(-4), 100, 140);
+   */
 }
 
 // Code based on http://www.emanueleferonato.com/2011/07/14/create-a-terrain-like-the-one-in-tiny-wings-with-flash-and-box2d/
@@ -591,7 +734,7 @@ function calcValleys(numberOfValleys, pixelStep) {
    for (var i = 0; i < numberOfValleys; i++) {
 
       // This was originally designed to generate hills, but for my golf game, valleys make more sense
-      var randomHeight = - Math.random() * maxHillHeight;
+      var randomHeight = -rand(10, maxHillHeight);
 
       if(i != 0) {
          hillStartY -= randomHeight;
@@ -615,9 +758,11 @@ function calcValleys(numberOfValleys, pixelStep) {
          }
 
          // Prevent values past the end of the world from being added to the array
-         if (j * pixelStep + hillWidth * i < game.world.width) {
+         if (hillX < game.world.width) {
             // Add the array of polygons to the collection being returned
             ret.push(poly);
+         } else {
+            break;
          }
 
       }
@@ -626,4 +771,12 @@ function calcValleys(numberOfValleys, pixelStep) {
 
    // Return the coordinates we've created
    return ret;
+}
+
+function rand(min, max) {
+   return min + (Math.random() * (max - min));
+}
+
+function randInt(min, max) {
+   return Math.round(rand(min, max));
 }
